75fe644e2d217ce1208c8f9905a65b729cb7ae72
oolong  tea <gogo34781@gmail.com>
2023-12-10 14:50:28 +0800

    Create classes.py

---
+++ core/classes.py
@@ -0,0 +1,9 @@
+from discord.ext import commands
+
+class Cog_Extension(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+class SelectMenu(commands.Cog):
+    def __init__(self,bot):
+        self.bot=bot
